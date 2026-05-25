6f4b8b5b7e7786fb98b7012fa120f5882daa3da9
hishambinraheem <hishambinraheem@gmail.com>
2026-05-25 15:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 150.1 MB (46.25%) - LeetSync

---
+++ 27-remove-element/remove-element.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int removeElement(List<int> nums, int val) {
+    int r=0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i] !=val){
+                nums[r]=nums[i];
+                r++;
+        }
+    }
+    return r;
+  }
+}
