30a73c06f28caa3c32f75f8ac060d589daf3eaac
hishambinraheem <hishambinraheem@gmail.com>
2026-07-15 10:09:35 +0530

    Time: 3 ms (0.00%) | Memory: 148.9 MB (81.82%) - LeetSync

---
+++ 812-rotate-string/rotate-string.dart
@@ -0,0 +1,9 @@
+class Solution {
+  bool rotateString(String s, String goal) {
+    if(s.length!=goal.length){
+        return false;
+    }
+    String doubleString=s+s;
+    return doubleString.contains(goal);
+  }
+}
